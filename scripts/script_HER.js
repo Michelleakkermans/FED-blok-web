fd5592ab836a9bf311d76a637b976dd31385a649
Michelleakkermans <90249520+Michelleakkermans@users.noreply.github.com>
2021-11-11 09:59:43 +0100

    Add files via upload

---
+++ scripts/script_HER.js
@@ -0,0 +1,18 @@
+
+document.querySelector('#darkmode').addEventListener('change', function(event){
+    document.querySelector('body').classList.toggle('dark');
+  });
+  
+  /* klikken met de muis op deButton*/
+  var deButton = document.querySelector(".Menu");
+  
+  function doeFormNeerEnOp(){
+      let hetFormulier = document.querySelector("form");
+      hetFormulier.classList.toggle("toonForm");
+  }
+  
+  deButton.addEventListener("click", doeFormNeerEnOp);
+  
+  /* einde van je code */
+  /*********************/
+  
